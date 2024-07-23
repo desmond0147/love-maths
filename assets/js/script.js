9a1f8e841e81668d253c2e59f4712af48e6d6984
Omonuwa Desmond Osaigbovo <omonuwaosai@gmail.com>
2024-07-23 01:18:03 +0000

    Add eventlistner for document loading and buttoncliks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
-console.log("connected!")
+//wait for the DOM to finish loading before running the game
+// Get the button element and add event listeners to them 
+
+document.addEventListener("DOMContentLoaded", function(){
+let buttons= document.getElementsByTagName("button");
+
+for(let button of buttons){
+    button.addEventListener("click", function(){
+        if(this.getAttribute("data-type")==="submit"){
+            alert("You clciked submit!");
+        } else{
+            let gameType= this.getAttribute("data-type");
+            alert(`You clicked ${gameType}`);
+        }
+    })
+}
+
+})
+
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+}
+ function incrementScore() {
+}
+
+function incrementWrongAnswer() {
+}
+function displayAdditionQuestion() {
+}
+function displaySubtractionQuestion() {
+}
+function displayMultiplicationQustion() {
+}
